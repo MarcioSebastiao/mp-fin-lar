6b4913fefbc5d1ecc2d6b4480e83d5b4eac33999
marciosebastiao <marcio.sx@outlook.com>
2026-02-11 10:41:50 -0300

    refactor: transformar maincontroller em classe base

--- src/backend/MpFinLar.API/Controllers/MainController.cs
+++ src/backend/MpFinLar.API/Controllers/MainController.cs
@@ -2,13 +2,25 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace MpFinLar.API.Controllers;
 
+/// <summary>
+/// Classe base para os controllers
+/// </summary>
 [ApiController]
 [Route("api/[controller]")]
-public class MainController : ControllerBase
+public abstract class MainController : ControllerBase
 {
-    [HttpGet()]
-    public ActionResult Get()
+
+    /// <summary>
+    /// Retorna um BadRequest contendo as mensagens de erro
+    /// </summary>
+    /// <param name="MensagensDeErros">
+    /// Coleção de mensagens de erro a serem retornadas na resposta.
+    /// </param>
+    protected ActionResult RespostaDeErro(IEnumerable<string> MensagensDeErros)
     {
-        return Ok("Main");
+        return BadRequest(new ValidationProblemDetails(new Dictionary<string, string[]>
+            {
+                {"Mensagens", MensagensDeErros.ToArray()}
+            }));
     }
 }
