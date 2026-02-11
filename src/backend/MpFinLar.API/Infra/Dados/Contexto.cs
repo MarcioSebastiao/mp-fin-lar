fe6f6b720596ac6bcfa61377d76835d5526dcbd1
marciosebastiao <marcio.sx@outlook.com>
2026-02-11 10:04:55 -0300

    feat: Criar contexto do banco de dados

---
+++ src/backend/MpFinLar.API/Infra/Dados/Contexto.cs
@@ -0,0 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using MpFinLar.API.Dominio.Entidades;
+
+namespace MpFinLar.API.Infra.Dados;
+
+/// <summary>
+/// Classe que representa o contexto do banco de dados da aplicação.
+/// Responsável por gerenciar as entidades e a conexão com o banco
+/// </summary>
+public sealed class Contexto : DbContext
+{
+    /// <summary>
+    /// Conjunto de entidades Pessoa que será mapeado para a tabela correspondente no banco de dados.
+    /// </summary>
+    public DbSet<Pessoa> Pessoas { get; set; }
+
+    public Contexto(DbContextOptions<Contexto> options) : base(options)
+    {
+
+    }
+}
--- src/backend/MpFinLar.API/Program.cs
+++ src/backend/MpFinLar.API/Program.cs
@@ -1,9 +1,18 @@
+using Microsoft.EntityFrameworkCore;
 using MpFinLar.API.Configuracoes;
+using MpFinLar.API.Infra.Dados;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.ConfigurarSwagger();
+
+// Registra o DbContext Contexto no container de injeção de dependência
+builder.Services.AddDbContext<Contexto>(options =>
+        // Configura o Entity Framework Core para utilizar o PostgreSQL usando o provedor Npgsql
+        // Obtém a string de conexão "DefaultConnection" a partir do arquivo de configuração
+        options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 var app = builder.Build();
 
