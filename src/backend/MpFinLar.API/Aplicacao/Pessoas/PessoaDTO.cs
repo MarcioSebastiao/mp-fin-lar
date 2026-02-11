5133ee5d9444be8dbde8fa6b8add6b693a877eab
marciosebastiao <marcio.sx@outlook.com>
2026-02-11 13:30:31 -0300

    feat: adicionar maxlength no nome de pessoadto

--- src/backend/MpFinLar.API/Aplicacao/Pessoas/PessoaDTO.cs
+++ src/backend/MpFinLar.API/Aplicacao/Pessoas/PessoaDTO.cs
@@ -5,6 +5,7 @@ namespace MpFinLar.API.Aplicacao.Pessoas;
 public class PessoaDTO
 {
     [Required(ErrorMessage = "O nome é obrigatório.")]
+    [MaxLength(200, ErrorMessage = "O nome deve ter no máximo 200 caracteres.")]
     public string Nome { get; set; } = string.Empty;
     public int Idade { get; set; }
 }
