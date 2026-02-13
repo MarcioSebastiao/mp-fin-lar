8fb8b4c0a512cb13ed51555e4ba268e1314e7797
marciosebastiao <marcio.sx@outlook.com>
2026-02-13 14:22:13 -0300

    feat: listar pessoas no frontend

--- src/frontend/MpFinLar.Web/src/paginas/pessoas/Pessoas.tsx
+++ src/frontend/MpFinLar.Web/src/paginas/pessoas/Pessoas.tsx
@@ -1,10 +1,24 @@
 import CriarPessoa from "../../componentes/Pessoa/CriarPessoa";
 import Modal from "../../componentes/Modal";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./Pessoas.css";
+import { obterPessoas } from "../../servicos/pessoasServico";
+import type { Pessoa } from "../../modelos/Pessoa";
 
 function Pessoas() {
     const [modalAberto, setModalAberto] = useState(false);
+
+    const [pessoas, setPessoas] = useState<Pessoa[]>([]);
+
+    useEffect(() => {
+        async function carregarPessoas() {
+            try {
+                setPessoas(await obterPessoas());
+            } catch (erro) {}
+        }
+        carregarPessoas();
+    }, []);
+
     return (
         <>
             <Modal aberto={modalAberto} aoFechar={() => setModalAberto(false)}>
@@ -13,6 +27,30 @@ function Pessoas() {
             <div className="container">
                 <div>
                     <button onClick={() => setModalAberto(true)}>Nova Pessoa</button>
+                </div>
+
+                <div className="lista-pessoas">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Nome</th>
+                                <th>Idade</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {pessoas.map((pessoa) => (
+                                <tr>
+                                    <td>{pessoa.nome}</td>
+                                    <td>{pessoa.idade}</td>
+                                    <td>
+                                        <button className="editar">Editar</button>
+                                        <button className="excluir">Excluir</button>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </>
--- src/frontend/MpFinLar.Web/src/servicos/pessoasServico.ts
+++ src/frontend/MpFinLar.Web/src/servicos/pessoasServico.ts
@@ -1,6 +1,11 @@
 import api from "./api";
-import type { CriarPessoaDto } from "../modelos/Pessoa";
+import type { CriarPessoaDto, Pessoa } from "../modelos/Pessoa";
 
 export async function criarPessoa(dados: CriarPessoaDto): Promise<void> {
     await api.post("/api/pessoa", dados);
+}
+
+export async function obterPessoas(): Promise<Pessoa[]> {
+    const response = await api.get("/api/pessoa");
+    return response.data as Pessoa[];
 }
