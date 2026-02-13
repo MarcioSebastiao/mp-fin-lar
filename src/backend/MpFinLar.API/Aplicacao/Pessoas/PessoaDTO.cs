888cb0c4bbedadb45b2b852b481a4217f6538613
marciosebastiao <marcio.sx@outlook.com>
2026-02-13 11:35:00 -0300

    feat: validar idade

--- src/backend/MpFinLar.API/Aplicacao/Pessoas/PessoaDTO.cs
+++ src/backend/MpFinLar.API/Aplicacao/Pessoas/PessoaDTO.cs
@@ -7,5 +7,7 @@ public class PessoaDTO
     [Required(ErrorMessage = "O nome é obrigatório.")]
     [MaxLength(200, ErrorMessage = "O nome deve ter no máximo 200 caracteres.")]
     public string Nome { get; set; } = string.Empty;
+    
+    [Range(0, int.MaxValue, ErrorMessage = "Informe uma idade valida.")]
     public int Idade { get; set; }
 }
