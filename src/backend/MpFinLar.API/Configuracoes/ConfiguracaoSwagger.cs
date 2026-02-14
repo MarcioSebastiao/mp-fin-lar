4dad2a96698638acfa5291fcda3a8190b5e512d7
marciosebastiao <marcio.sx@outlook.com>
2026-02-14 08:04:34 -0300

    feat: configurar comentarios xml swagger

--- src/backend/MpFinLar.API/Configuracoes/ConfiguracaoSwagger.cs
+++ src/backend/MpFinLar.API/Configuracoes/ConfiguracaoSwagger.cs
@@ -1,3 +1,6 @@
+using System.Reflection;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
 namespace MpFinLar.API.Configuracoes;
 
 public static class ConfiguracaoSwagger
@@ -5,6 +8,13 @@ public static class ConfiguracaoSwagger
     public static void ConfigurarSwagger(this IServiceCollection services)
     {
         services.AddEndpointsApiExplorer();
-        services.AddSwaggerGen();
+        services.AddSwaggerGen(c => ConfigurarComentariosXmlSwagger(c));
+    }
+
+    private static void ConfigurarComentariosXmlSwagger(SwaggerGenOptions options)
+    {
+        var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+        var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+        options.IncludeXmlComments(xmlPath);
     }
 }
