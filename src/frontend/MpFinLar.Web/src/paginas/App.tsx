457229cde257858e654bcae58f6a08c72979619a
marciosebastiao <marcio.sx@outlook.com>
2026-02-14 17:55:03 -0300

    feat: criar barra de navegação

--- src/frontend/MpFinLar.Web/src/paginas/App.tsx
+++ src/frontend/MpFinLar.Web/src/paginas/App.tsx
@@ -1,12 +1,6 @@
-import { Link } from "react-router-dom";
-
 function App() {
     return (
         <>
-            <div className="app">
-                <Link to="/Pessoas">Pessoas</Link>
-                <Link to="/Categorias">Categorias</Link>
-            </div>
         </>
     );
 }
--- src/frontend/MpFinLar.Web/src/rotas/Rotas.tsx
+++ src/frontend/MpFinLar.Web/src/rotas/Rotas.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { Route, Routes, BrowserRouter, Link } from "react-router-dom";
 import App from "../paginas/App";
 import Pessoas from "../paginas/pessoas/Pessoas";
 import Categorias from "../paginas/Categorias/Categorias";
@@ -10,6 +10,11 @@ import Categorias from "../paginas/Categorias/Categorias";
 export default function Rotas() {
     return (
         <BrowserRouter>
+            <nav>
+                <Link to="/pessoas">Pessoas</Link>
+                <Link to="/categorias">Categorias</Link>
+            </nav>
+            
             <Routes>
                 <Route path="/" element={<App />} />
                 <Route path="/pessoas" element={<Pessoas />} />
