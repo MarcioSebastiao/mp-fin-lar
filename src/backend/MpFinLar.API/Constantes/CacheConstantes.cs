a8d3b1a84e0572500a2644a2bd1ed95ee1738450
marciosebastiao <marcio.sx@outlook.com>
2026-02-18 18:52:41 -0300

    feat: implementar cache para pessoas

--- src/backend/MpFinLar.API/Constantes/CacheConstantes.cs
+++ src/backend/MpFinLar.API/Constantes/CacheConstantes.cs
@@ -3,5 +3,6 @@ namespace MpFinLar.API.Constantes;
 public sealed class CacheConstantes
 {
     public const string CategoriasRespostaCacheKey = "categorias_resposta";
+    public const string PessoasRespostaCacheKey = "pessoas_resposta";
 
 }
--- src/backend/MpFinLar.API/Controllers/PessoaController.cs
+++ src/backend/MpFinLar.API/Controllers/PessoaController.cs
@@ -1,38 +1,44 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
 using MpFinLar.API.Aplicacao;
 using MpFinLar.API.Aplicacao.Pessoas;
+using MpFinLar.API.Constantes;
 
 namespace MpFinLar.API.Controllers;
 
 public sealed class PessoaController : MainController
 {
     private readonly IPessoaAplicacao _aplicacao;
+    private readonly IMemoryCache _cache;
 
-    public PessoaController(IPessoaAplicacao aplicacao)
+    public PessoaController(IPessoaAplicacao aplicacao, IMemoryCache cache)
     {
         _aplicacao = aplicacao;
+        _cache = cache;
     }
 
     [HttpPost]
     public async Task<ActionResult> Criar(PessoaDTO pessoaDTO)
     {
         (PessoaRespostaDTO? pessoa, ResultadoAplicacao resultado) = await _aplicacao.CriarAsync(pessoaDTO);
+        if (!resultado.Sucesso)
+            return RespostaDeErro(resultado.Notificacoes);
 
-        if (resultado.Sucesso)
-            return Ok(pessoa);
+        _cache.Remove(CacheConstantes.PessoasRespostaCacheKey);
 
-        return RespostaDeErro(resultado.Notificacoes);
-
+        return Ok(pessoa);
     }
 
     [HttpPut("{id:guid}")]
     public async Task<ActionResult> Atualizar(Guid id, PessoaDTO pessoaDTO)
     {
         (PessoaRespostaDTO? pessoa, ResultadoAplicacao resultado) = await _aplicacao.AtualizarAsync(id, pessoaDTO);
-        if (resultado.Sucesso)
-            return Ok(pessoa);
+        if (!resultado.Sucesso)
+            return RespostaDeErro(resultado.Notificacoes);
 
-        return RespostaDeErro(resultado.Notificacoes);
+        _cache.Remove(CacheConstantes.PessoasRespostaCacheKey);
+
+        return Ok(pessoa);
     }
 
 
@@ -40,16 +46,23 @@ public sealed class PessoaController : MainController
     public async Task<ActionResult> Remover(Guid id)
     {
         var resultado = await _aplicacao.RemoverAsync(id);
-        if (resultado.Sucesso)
-            return Ok();
+        if (!resultado.Sucesso)
+            return RespostaDeErro(resultado.Notificacoes);
 
-        return RespostaDeErro(resultado.Notificacoes);
+        _cache.Remove(CacheConstantes.PessoasRespostaCacheKey);
+
+        return Ok();
     }
 
     [HttpGet]
     public async Task<ActionResult<PessoasRespostaDTO>> Obter(int pularItens, int quantidadeItens)
     {
-        var pessoas = await _aplicacao.ObterAsync(pularItens, quantidadeItens);
+        if (_cache.TryGetValue(CacheConstantes.PessoasRespostaCacheKey, out PessoasRespostaDTO? pessoas))
+            return pessoas!;
+
+        pessoas = await _aplicacao.ObterAsync(pularItens, quantidadeItens);
+        _cache.Set(CacheConstantes.PessoasRespostaCacheKey, pessoas, TimeSpan.FromMinutes(10));
+
         return Ok(pessoas);
     }
 }
